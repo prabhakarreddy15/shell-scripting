c6fb3d1481bc5c2ce904f69b444b6b6909dc5d48
prabhakar <pp19abj@herts.ac.uk>
2021-09-02 11:51:10 +0100

    echo comand with new line

--- 01-shell.sh
+++ 01-shell.sh
@@ -3,6 +3,4 @@
 ## hello
 
 
-## working to gether is importent to every one in project it makes you to get perfect in delivering confident with project.
-
-echo -e "hello world  ,/n/t improve your skils/n/t"
+echo -e hello world  ,/n/t improve your skils/n/t
