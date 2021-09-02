043f6d1cb3d2b85f07b891c69e75383934283ea1
prabhakar <pp19abj@herts.ac.uk>
2021-09-02 16:52:47 +0100

    variable declaration with dynamic value and static value. CORECTED MISTAKES

--- 03-variable.sh
+++ 03-variable.sh
@@ -4,17 +4,17 @@ A=10
 
 echo A = $A
 
-DATE = "02-09-2021"
+DATE="02-09-2021"
 echo welcome, today date is $DATE
 
 ## comand subs
 
-NO_OF_USERS = $(who | wc -l)
+NO_OF_USERS=$(who | wc -l)
 
-echo NO_OF_USERS = $NO_OF_USERS
+echo "NO_OF_USERS=$NO_OF_USERS"
 
 ## date with command substitution
 
-DATE =$(date +%F)
-echo welcome, today date is $DATE
+DATE=$(date +%F)
+echo "welcome, today date is $DATE"
 
