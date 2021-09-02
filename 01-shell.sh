886434c813e46f6430d18bf45aa5f77e39ebad45
prabhakar <pp19abj@herts.ac.uk>
2021-09-02 11:47:42 +0100

    echo comand with new line

--- 01-shell.sh
+++ 01-shell.sh
@@ -1,4 +1,8 @@
+#!/usr/bin/bash
+
 ## hello
 
 
-working to gether is importent to every one in project it makes you to get perfect in delivering confident with project.
+## working to gether is importent to every one in project it makes you to get perfect in delivering confident with project.
+
+echo -e "hello world /n/t/t improve your skils"
