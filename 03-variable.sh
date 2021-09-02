74ca83b4ee6b5e05d6ad02f28d0f9615fd2b5717
prabhakar <pp19abj@herts.ac.uk>
2021-09-02 16:24:53 +0100

    variable declaration with dynamic value and static value

---
+++ 03-variable.sh
@@ -0,0 +1,20 @@
+#!/urs/bin/bash
+
+A=10
+
+echo A = $A
+
+DATR = "02-09-2021"
+echo welcome, today date is $DATE
+
+## comand subs
+
+NO_OF_USERS = $(who | wc -l)
+
+echo NO_OF_USERS = $NO_OF_USERS
+
+## date with command substitution
+
+DATE =$(date +%F)
+echo welcome, today date is $DATE
+
